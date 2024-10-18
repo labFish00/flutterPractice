0da7e1565fb6cd94b9db4768cf268bcf47df0b2f
boxfish <boxfish893@gmail.com>
2024-10-18 17:14:07 +0900

    feat: part4終了

--- pokemon/lib/poke_detail.dart
+++ pokemon/lib/poke_detail.dart
@@ -33,8 +33,14 @@ class PokeDetail extends StatelessWidget {
             "pikachu",
             style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
           ),
-          const Chip(
-            label: Text("electric"),
+          Chip(
+            label: Text(
+              "electric",
+              style: TextStyle(
+                  color: Colors.yellow.computeLuminance() > 0.5
+                      ? Colors.black
+                      : Colors.white),
+            ),
             backgroundColor: Colors.yellow,
           )
         ],
