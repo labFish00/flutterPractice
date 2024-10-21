ed07bb9660c3a0572a3c67d65f6978388f7039f5
boxfish <boxfish893@gmail.com>
2024-10-22 04:52:28 +0900

    refactor: pokeListItemsをファイル分けした

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './poke_detail.dart';
+import 'poke_list_items.dart';
 
 void main() {
   runApp(const MyApp());
@@ -80,38 +80,6 @@ class Settings extends StatelessWidget {
           title: Text('Dark/Light Mode'),
         ),
       ],
-    );
-  }
-}
-
-class PokeListItem extends StatelessWidget {
-  final int index;
-  const PokeListItem({super.key, required this.index});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        width: 80,
-        decoration: BoxDecoration(
-          color: Colors.yellow.withOpacity(.5),
-          borderRadius: BorderRadius.circular(10),
-          image: const DecorationImage(
-              image: NetworkImage(
-                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
-              fit: BoxFit.fitWidth),
-        ),
-      ),
-      title: const Text(
-        "pikachu",
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      subtitle: const Text("electric"),
-      trailing: const Icon(Icons.navigate_next),
-      onTap: () => {
-        Navigator.of(context).push(MaterialPageRoute(
-            builder: (BuildContext context) => const PokeDetail()))
-      },
     );
   }
 }
