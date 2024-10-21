d6c45c5ab9f2a8361140c50dfbd1cae1513c790e
boxfish <boxfish893@gmail.com>
2024-10-21 16:42:35 +0900

    step5: 終了

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -21,16 +21,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({super.key});
+
+  @override
+  TopPageState createState() => TopPageState();
+}
+
+class TopPageState extends State<TopPage> {
+  int currentbnb = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-      itemCount: 10000,
+      body: Scaffold(
+        body: SafeArea(
+            child: currentbnb == 0 ? const PokeList() : const Settings()),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+            () => currentbnb = index,
+          )
+        },
+        currentIndex: currentbnb,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'settings'),
+        ],
+      ),
+    );
+  }
+}
+
+class PokeList extends StatelessWidget {
+  const PokeList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+      itemCount: 1010,
       itemBuilder: (context, index) => PokeListItem(index: index),
-    ));
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        ListTile(
+          leading: Icon(Icons.lightbulb),
+          title: Text('Dark/Light Mode'),
+        ),
+      ],
+    );
   }
 }
 
